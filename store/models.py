696da6fc165a959f0afa5b6f41bfa4ebc70b43cb
F4rab1 <issafarabi2022@gmail.com>
2024-04-15 23:28:57 +0500

    PostgreSQL connection

--- store/models.py
+++ store/models.py
@@ -1,17 +1,26 @@
 from django.db import models
 
 
+class Promotion(models.Model):
+    description = models.CharField(max_length=255)
+    discount = models.FloatField()
+    # product_set
+
+
 class Collection(models.Model):
     title = models.CharField(max_length=255)
+    featured_product = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True, related_name='+')
 
 
 class Product(models.Model):
     title = models.CharField(max_length=255) 
+    slug = models.SlugField()
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
+    promotions = models.ManyToManyField(Promotion)
 
 
 class Customer(models.Model):
--- storefront2/settings.py
+++ storefront2/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = 'storefront2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'storefront2',
+        'USER': 'postgres',
+        'PASSWORD': 'isaf',
+        'HOST': 'localhost', 
+        'PORT': '5432', 
     }
 }
 
