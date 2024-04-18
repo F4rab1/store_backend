94f056c0e36e7cce7ad8fbd921eb2f89534aee1d
F4rab1 <issafarabi2022@gmail.com>
2024-04-18 18:40:49 +0500

    Practiced ORM

--- playground/views.py
+++ playground/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.db.models import Q, F
 from store.models import Product
+from store.models import OrderItem
 
 
 def say_hello(request):
@@ -25,7 +26,10 @@ def say_hello(request):
     # query_set = Product.objects.order_by('unit_price', '-title').reverse()
     # product = Product.objects.order_by('unit_price')[0]
     # product = Product.objects.earliest('unit_price')
-    query_set = Product.objects.all()[5:10]
+    # query_set = Product.objects.all()[5:10]
+    # query_set = OrderItem.objects.values('product_id', 'product__title').distinct().order_by('product__title')
+    # query_set = Product.objects.filter(id__in=OrderItem.objects.values('product_id').distinct()).order_by('title')
+    # query_set = Product.objects.only('id', 'title')
+    query_set = Product.objects.defer('description')
 
-    
     return render(request, 'hello.html', { 'name': 'Farabi', 'products': list(query_set)})
