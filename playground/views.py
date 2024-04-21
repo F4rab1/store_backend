539112ba315d0c5711b0d91295074d1174a23ad1
F4rab1 <issafarabi2022@gmail.com>
2024-04-21 18:19:19 +0500

    Practiced ORM

--- playground/views.py
+++ playground/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.db.models import Q, F
-from store.models import Product
+from django.db.models import Q, F, Func, Value, ExpressionWrapper, DecimalField
+from django.db.models.functions import Concat
+from django.db.models.aggregates import Count, Max, Min, Avg, Sum
+from store.models import Product, Order, Collection, Customer
 from store.models import OrderItem
 
 
@@ -32,7 +34,30 @@ def say_hello(request):
     # query_set = Product.objects.only('id', 'title')
     # query_set = Product.objects.defer('description')
     # query_set = Product.objects.select_related('collection').all()
-    query_set = Product.objects.prefetch_related('promotions').all()
-    
+    # query_set = Product.objects.prefetch_related('promotions').all()
+    # query_set = Order.objects.select_related('customer').prefetch_related('orderitem_set__product').order_by('-placed_at')[:5]
+    # result = Product.objects.filter(collection_id=1).aggregate(count=Count('id'), min_price=Min('unit_price'))
+    # query_set = Product.objects.annotate(discounted_price=F('unit_price') * 0.9)
+    # query_set = Customer.objects.annotate(
+    #     full_name=Func(F('first_name'), Value(' '), F('last_name'), function='CONCAT')
+    # )
+    # query_set = Customer.objects.annotate(
+    #     full_name=Concat('first_name', Value(' '), 'last_name')
+    # )
+    # query_set = Customer.objects.annotate(
+    #     orders=Count('order')
+    # )
+    # discounted_price = ExpressionWrapper(F('unit_price') * 0.9, output_field=DecimalField())
+    # query_set = Product.objects.annotate(discounted_price=discounted_price)
+    # query_set = Customer.objects.annotate(last_order_id=Max('order__id'))
+    # query_set = Product. objects.all()
+    # list (query_set)
+    # query_set [0]
+    # collection = Collection()
+    # collection.title = 'Video Games'
+    # collection.featured_product = Product(pk=1)
+    # collection.save()
+    collection = Collection.objects.create(title='Video Games', featured_product=Product.objects.get(pk=1))
+    collection.save()
 
-    return render(request, 'hello.html', { 'name': 'Farabi', 'products': list(query_set)})
+    return render(request, 'hello.html', { 'name': 'Farabi' })
