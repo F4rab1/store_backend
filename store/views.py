d4e470d5b04efe990f704380b8ed45be052993fe
F4rab1 <issafarabi2022@gmail.com>
2024-04-25 10:56:48 +0500

    Serializers for product

--- store/views.py
+++ store/views.py
@@ -2,12 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from store.models import Product
+from .serializers import ProductSerializer
 
 # Create your views here.
 @api_view()
 def product_list(request):
-    return Response('ok')
+    queryset = Product.objects.all()
+    serializer = ProductSerializer(queryset, many=True)
+    return Response(serializer.data)
 
 @api_view()
-def product_detail(reques, id):
-    return Response(id)
+def product_detail(request, id):
+    product = Product.objects.get(pk=id)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
