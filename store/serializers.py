af9258d0afc0507d708ce7d0f40d8898363a6190
F4rab1 <issafarabi2022@gmail.com>
2024-07-02 23:33:11 +0500

    changes

--- store/serializers.py
+++ store/serializers.py
@@ -8,15 +8,12 @@ class CollectionSerializer(serializers.Serializer):
     title = serializers.CharField(max_length=255)
 
 
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
-    price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'unit_price', 'price_with_tax', 'collection']
+
     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
-    # collection = serializers.PrimaryKeyRelatedField(
-    #     queryset=Collection.objects.all()
-    # )
-    collection = CollectionSerializer()
 
     def calculate_tax(self, product: Product):
         return product.unit_price * Decimal(1.1)
--- store/views.py
+++ store/views.py
@@ -1,19 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from store.models import Product
-from .serializers import ProductSerializer
+from store.models import Product, Collection
+from .serializers import ProductSerializer, CollectionSerializer
 
 # Create your views here.
-@api_view()
+@api_view(['GET', 'POST'])
 def product_list(request):
-    queryset = Product.objects.select_related('collection').all()
-    serializer = ProductSerializer(queryset, many=True)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        queryset = Product.objects.select_related('collection').all()
+        serializer = ProductSerializer(queryset, many=True, context={'request': request})
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        print(serializer.validated_data)
+        return Response('ok')
 
 @api_view()
 def product_detail(request, id):
     product = Product.objects.get(pk=id)
-    serializer = ProductSerializer(product)
+    serializer = ProductSerializer(product, context={'request': request})
+    return Response(serializer.data)
+
+@api_view()
+def collection_detail(request, pk):
+    collection = Collection.objects.get(pk=pk)
+    serializer = CollectionSerializer(collection)
     return Response(serializer.data)
