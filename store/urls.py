be5ffb905e66d8693a85e570da08ce2894a11cf6
F4rab1 <issafarabi2022@gmail.com>
2024-07-21 19:34:51 +0500

    using product id when retrieving reviews

--- store/urls.py
+++ store/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
-from rest_framework.routers import SimpleRouter
+from rest_framework_nested import routers
 
-router = SimpleRouter()
+router = routers.DefaultRouter()
 router.register('products', views.ProductViewSet)
 router.register('collection', views.CollectionViewSet)
 
-urlpatterns = router.urls
+products_router = routers.NestedDefaultRouter(router, 'products', lookup='product')
+products_router.register('reviews', views.ReviewViewSet, basename='product-reviews')
+
+urlpatterns = router.urls + products_router.urls
