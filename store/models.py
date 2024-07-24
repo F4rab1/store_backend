f637cd96d658b5c4c0900dce3f0133239cceb9cb
F4rab1 <issafarabi2022@gmail.com>
2024-07-24 18:39:58 +0500

    Getting a Cart with cart items implemented

--- store/models.py
+++ store/models.py
@@ -100,6 +100,7 @@ class CartItem(models.Model):
     class Meta:
         unique_together = [['cart', 'product']]
 
+
 class Review(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews')
     name = models.CharField(max_length=255)
